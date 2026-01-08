3b50df4ea4057bedcf1a7ad1de16d838d6045c30
ShushilShah <shahshushil3@gmail.com>
2026-01-08 13:19:47 +0545

    signup view code

--- lms_app/views.py
+++ lms_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from smart_lms.forms import SignupForm
+from django.contrib import messages
+from django.shortcuts import render, redirect
 # Create your views here.
+
+def singup_view(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Your account has been created successfully! You can now login.")
+            return redirect("login")
+        
+        else:
+            form = SignupForm()
+
+        context = {
+            "form": form
+        }
+        return render(request, "accounts/signup.html", context)
