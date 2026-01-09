ffc3d67834a1de4530b8812c4d7f54257cbbe83d
ShushilShah <shahshushil3@gmail.com>
2026-01-09 22:03:42 +0545

    remember me field added and logout also

--- lms_app/views.py
+++ lms_app/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .models import UserProfile
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def signup_view(request):
@@ -32,11 +32,18 @@ def login_view(request):
     if request.method == 'POST':
         username = request.POST.get("username")
         password = request.POST.get("password")
+        remember_me = request.POST.get("remember_me")
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
+
+            # session base expiry logic
+            if not remember_me:
+                request.session.set_expiry(0)
+            else:
+                request.session.set_expiry(1209600) # 2 weeks
 
             # role based redirect
             role = user.userprofile.role
@@ -52,3 +59,7 @@ def login_view(request):
 
     return render(request, "accounts/login.html")
 
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
