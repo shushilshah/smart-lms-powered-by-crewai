80aa5f18d952fa07016f5de36fd4ff18d350f9bf
ShushilShah <shahshushil3@gmail.com>
2026-02-07 14:40:20 +0545

    url for landing page

--- smart_lms/urls.py
+++ smart_lms/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="landing_page.html"), name="landing_page"),
     path("admin/", admin.site.urls),
     path("accounts/", include("lms_app.urls")),
 ]
