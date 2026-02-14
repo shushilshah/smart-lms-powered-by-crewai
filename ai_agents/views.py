26e5c5102c080df4953d6ce8be4b3264aa471298
ShushilShah <shahshushil3@gmail.com>
2026-02-14 13:16:58 +0545

    AI can create course successfully in mode deep

--- ai_agents/views.py
+++ ai_agents/views.py
@@ -3,6 +3,7 @@ import json
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from ai_agents.services.run_course_agent import run_course_planner
+from lms_app.models import Course
 
 @csrf_exempt
 def generate_course(request):
@@ -15,7 +16,34 @@ def generate_course(request):
                 return JsonResponse({"error": "Prescription is required"}, status=400)
             
             result  = run_course_planner(prescription)
-            return JsonResponse(result, safe=False)
+            level_value = result.get("level", "beginner").lower()
+            outcomes = result.get("learning_outcomes", [])
+            if isinstance(outcomes, list):
+                outcomes_text = "\n".join(outcomes)
+            else:
+                outcomes_text = str(outcomes)
+
+            course = Course.objects.create(
+                title = result.get("title"),
+                description = result.get("description"),
+                # student_slots = result.get("student_slots"),
+                level = level_value,
+                duration = result.get("duration"),
+                teacher = request.user,
+                learning_outcomes = outcomes_text,
+                is_published = False
+
+            )
+
+            return JsonResponse({
+                "id": course.id,
+                "title": course.title,
+                "description": course.description,
+                "level": course.level,
+                "duration": course.duration,
+                "learning_outcomes": outcomes_text,
+                "message": "Saved to database successfully"
+            })
         
         except Exception as e:
             return JsonResponse({"error": str(e)}, status=500)
